f8c29d38ed08905e6de25e875a8e75b5b6a5b723
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-03-23 14:13:06 +0300

    five commit

--- src/js/user.js
+++ src/js/user.js
@@ -5,8 +5,5 @@ export default function checkHealthStatus(player) {
   if (player.health > 15 && player.health < 50) {
     return 'wounded';
   }
-  if (player.health <= 15) {
-    return 'critical';
-  }
-  return 0;
+  return 'critical';
 }
